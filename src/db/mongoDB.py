ba0718f7ee2333fca854c4dc10af66da321bf208
Nisarg Gandhi <nisarggandhi21@gmail.com>
2024-10-16 17:41:21 +0530

    Connecting and Storing Data in MongoDB

---
+++ src/db/mongoDB.py
@@ -0,0 +1,29 @@
+import json
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+MONGO_URI = os.getenv("DB_URL")
+client = MongoClient(MONGO_URI)
+db = client.EventsDB
+
+try:
+  client.admin.command("ping")
+  print("You successfully connected to MongoDB!")
+except Exception as e:
+  print(e)
+
+
+def insert_person_event(json_data: str):
+  eventsCollection = db["PersonEvents"]
+  personDocument = json.loads(json_data)
+  eventsCollection.insert_one(personDocument)
+
+
+def insert_jobs(json_data: str):
+  taskCollection = db["Jobs"]
+  taskDocument = json.loads(json_data)
+  taskCollection.insert_one(taskDocument)
